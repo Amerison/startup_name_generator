eb4576de649432bd334ee3bf58744bdeb4600bdc
amerison <amerison@utexas.edu>
2022-01-08 14:07:23 -0600

    Favorite a startup name
    Adds ability to favorite a specific startup name

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,8 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[]; //Creates a list
+  final _saved =
+      <WordPair>{}; // {} is a set where objects within it only occur once
   final _biggerFont =
       const TextStyle(fontSize: 18.0); //Makes something the same fontsize
 
@@ -30,6 +32,7 @@ class _RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.orange,
         title: const Text('Startup Name Generator'),
       ),
       body: _buildSuggestions(),
@@ -40,7 +43,7 @@ class _RandomWordsState extends State<RandomWords> {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0), // Creates padding
         itemBuilder: (context, i) {
-          // itemBuilder Function that takes in context and i
+          // itemBuilder Function that takes in context and i (place in the list)
           if (i.isOdd)
             return const Divider(); // if the place-holder is odd, then return Divider
 
@@ -53,11 +56,29 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved
+            ? Icons.favorite
+            : Icons
+                .favorite_border, // An if-else statement. If it's part of alreadySaved, then make it "favorite and heart filled", else make it just heart border
+        color: alreadySaved ? Colors.red : null, // Syntax
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
